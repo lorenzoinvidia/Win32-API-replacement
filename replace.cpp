91d55ed51a87c112aa880c93500f2554d4ccf7f3
Lorenzo Invidia <loreinvidia@gmail.com>
2020-10-22 11:50:11 +0200

    strcmp + wcscmp

--- replace.cpp
+++ replace.cpp
@@ -1,9 +1,24 @@
+/* **********************************************************************
+*  PoC: replace.cpp
+*  Author: lorenzoinvidia
+*
+*  Description:
+*  Shellcoding-like resolve function address without imports
+*
+* **********************************************************************
+* */
+
+
 /* 
     Adapted from https://github.com/zerosum0x0/LoadLibrary-GetProcAddress-Replacements
 */
 #include <Windows.h>
 #include <winternl.h>
+//#define DEBUG
+
+#ifdef DEBUG
 #include <stdio.h>
+#endif //DEBUG
 
 /* Return ptr to PEB */
 static __inline PEB __declspec(naked) __forceinline *GetPEB() {
@@ -14,14 +29,40 @@ static __inline PEB __declspec(naked) __forceinline *GetPEB() {
 }//GetPEB
 
 
+/*
+  stcmp like 
+  https://stackoverflow.com/questions/34873209/implementation-of-strcmp#34873406
+ */
+int eqstrcmp(char str1[], char str2[]) {
+    for (int i = 0; ; i++){
+        if (str1[i] != str2[i]) return str1[i] < str2[i] ? -1 : 1;
+        if (str1[i] == '\0') return 0;
+    }
+}//eqstrcmp
+
+ 
+/*
+  wcscmp like
+ */
+int eqwcsmp(const wchar_t *str1, const wchar_t *str2) {
+    for (int i = 0; ; i++) {
+        if (str1[i] != str2[i]) return str1[i] < str2[i] ? -1 : 1;
+        if (str1[i] == L'\0') return 0;
+    }
+}//eqwcsmp
+
+
+
 /* Get module handle like */
-HMODULE WINAPI xGetModuleHandle(LPCWSTR moduleName) {
+HMODULE WINAPI eqGetModuleHandle(LPCWSTR moduleName) {
 	PEB *pPeb = NULL;
 	LIST_ENTRY *pListEntry = NULL;
 	LDR_DATA_TABLE_ENTRY *pLdrDataTableEntry = NULL;
 
     if ( !(pPeb = GetPEB()) ){
+#ifdef DEBUG
         printf("PEB");
+#endif //DEBUG
         return NULL;
     }
 
@@ -29,11 +70,13 @@ HMODULE WINAPI xGetModuleHandle(LPCWSTR moduleName) {
 	pLdrDataTableEntry = (PLDR_DATA_TABLE_ENTRY)pPeb->Ldr->InMemoryOrderModuleList.Flink;
 	pListEntry = pPeb->Ldr->InMemoryOrderModuleList.Flink;
 	
-	do
-	{
-		if (lstrcmpiW(pLdrDataTableEntry->FullDllName.Buffer, moduleName) == 0)
-			return (HMODULE)pLdrDataTableEntry->Reserved2[0]; // handle
-
+	do {
+        if (eqwcsmp(pLdrDataTableEntry->FullDllName.Buffer, moduleName) == 0){   
+#ifdef DEBUG
+            printf("%ws\n", pLdrDataTableEntry->FullDllName.Buffer);
+#endif //DEBUG
+            return (HMODULE)pLdrDataTableEntry->Reserved2[0]; // handle
+        }
         // Next node
 		pListEntry = pListEntry->Flink;
 		pLdrDataTableEntry = (PLDR_DATA_TABLE_ENTRY)(pListEntry->Flink);
@@ -41,21 +84,25 @@ HMODULE WINAPI xGetModuleHandle(LPCWSTR moduleName) {
 	} while (pListEntry != pPeb->Ldr->InMemoryOrderModuleList.Flink);
 
 	return NULL;
-}//xGetModuleHandle
+}//eqGetModuleHandle
 
 
 /* Get procedure address like */
-FARPROC WINAPI xGetProcAddress(HMODULE hModule, const char *lpProcName){
+FARPROC WINAPI eqGetProcAddress(HMODULE hModule, char *lpProcName){
 
 	PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)hModule;
 	if ( IMAGE_DOS_SIGNATURE != pDosHeader->e_magic ){
+#ifdef DEBUG
         printf("DOS signature mismatch");
+#endif //DEBUG
         return NULL;
     }
     
     PIMAGE_NT_HEADERS pNtHeaders = (PIMAGE_NT_HEADERS)( (LPBYTE)hModule + pDosHeader->e_lfanew );
     if ( IMAGE_NT_SIGNATURE != pNtHeaders->Signature ){
+#ifdef DEBUG
         printf("PE signature mismatch");
+#endif //DEBUG
         return NULL;
     }
     
@@ -70,31 +117,31 @@ FARPROC WINAPI xGetProcAddress(HMODULE hModule, const char *lpProcName){
     PDWORD pAddress = (PDWORD)( (LPBYTE)hModule + pExportDirectory->AddressOfFunctions);
 
 	for(unsigned i=0; i<pExportDirectory->NumberOfNames; i++){
-        if( !strcmp(lpProcName,(char*)hModule+pNames[i]) ){
+        if( eqstrcmp(lpProcName,(char*)hModule+pNames[i]) == 0 ){
             return (FARPROC)((LPBYTE)hModule+pAddress[pOrdinals[i]]);
         }
     }
     return NULL;
-}//xGetProcAddress
+}//eqGetProcAddress
 
 typedef HMODULE(WINAPI * _LoadLibrary)(LPCSTR lpFileName);
 typedef int (WINAPI * _MessageBox)(
     HWND    hWnd,
-    LPCWSTR lpText,
-    LPCWSTR lpCaption,
+    LPCSTR lpText,
+    LPCSTR lpCaption,
     UINT    uType
 );
 
-//typedef FARPROC(WINAPI * _GetProcAddress)(HMODULE hModule, LPCSTR lpProcName);
+typedef FARPROC(WINAPI * _GetProcAddress)(HMODULE hModule, LPCSTR lpProcName);
 //typedef HMODULE(WINAPI * _GetModuleHandle)(LPCWSTR lpModuleName);
 
 int main(){
 
-    _LoadLibrary pLoadLibrary = (_LoadLibrary)xGetProcAddress(xGetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
-    //_GetProcAddress pGetProcAddress = (_GetProcAddress)xGetProcAddress(L"kernel32.dll", "GetProcAddress");
+    _LoadLibrary pLoadLibrary = (_LoadLibrary)eqGetProcAddress(eqGetModuleHandle(L"KERNEL32.DLL"), "LoadLibraryA");
+    _GetProcAddress pGetProcAddress = (_GetProcAddress)GetProcAddress(eqGetModuleHandle(L"KERNEL32.DLL"), "GetProcAddress");
     
-    _MessageBox pMessageBox = (_MessageBox)xGetProcAddress(pLoadLibrary("user32.dll"), "MessageBoxW");
-    pMessageBox(NULL, L"It works!", L"Hello World!", MB_OK);
+    _MessageBox pMessageBox = (_MessageBox)pGetProcAddress(pLoadLibrary("user32.dll"), "MessageBoxA");
+    pMessageBox(NULL, "World", "Hello", MB_OK);
 
     return 0;
 }
